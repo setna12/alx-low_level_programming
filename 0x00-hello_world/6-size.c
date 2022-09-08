eade11ee097c9d923e1c0d3db2fa321253d481d0
setna12 <hanamariam245@gmail.com>
2022-09-08 16:13:35 -0700

    task 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+/** 
+
+* main- print in the puts function
+
+*
+
+*desciption :main function 
+
+* this program prints all sentence within the quotation mark
+
+* Return : 0
+
+*/
+
+int main(void)
+
+{
+
+  char c;
+
+  int i;
+
+  long li;
+
+   long long lli;
+
+   float f;
+
+printf("Size of a char :%ld byte(s)\n" , sizeof(c));
+
+printf("Size of an int :%ld byte(s)\n" , sizeof(i));
+
+printf("Size of a long int :%ld byte(s)\n" , sizeof(li));
+
+printf("Size of a long long int :%ld byte(s)\n" , sizeof(lli));
+
+printf("Size of a float :%ld byte(s)\n" , sizeof(f));
+
+return(0);
+
+}
