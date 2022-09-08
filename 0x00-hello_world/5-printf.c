ae49c9506ae7e1b75ec36470f276e70c1d6e9138
setna12 <hanamariam245@gmail.com>
2022-09-08 15:58:51 -0700

    task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/** 
+
+* main- print in the puts function
+
+*
+
+*desciption :main function 
+
+* this program prints all sentence within the quotation mark
+
+* Return : 0
+
+*/
+
+int main(void)
+
+{
+
+  printf("with proper grammer, but the outcome is apiece of art, \n");
+
+   return (0);
+
+}
