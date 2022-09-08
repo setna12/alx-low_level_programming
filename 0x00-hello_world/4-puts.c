d574aa78497a519e33a69f618c9b074476f67040
setna12 <hanamariam245@gmail.com>
2022-09-08 15:46:52 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,23 @@
 #include <stdio.h >
-
+/** 
+ *
+ * * main
+ *
+ * *
+ *
+ * *desciption :main function 
+ *
+ * * this program prints all sentence within the quotation mark
+ *
+ * * Return :always 0(success)
+ *
+ * */
 int main(void)
 
 {
 
 	  puts("\" Programming is like building a multilingual puzzle");
 
-	  return(0);
+	  return (0);
 
 }
