d57c8577b1f04bc1c4e2f84185b8b1c1a4f96a9a
setna12 <hanamariam245@gmail.com>
2022-09-08 16:22:36 -0700

    task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -24,19 +24,19 @@ int main(void)
 
   long li;
 
-   long long lli;
+  long long int lli;
 
    float f;
 
-printf("Size of a char :%ld byte(s)\n" , sizeof(c));
+printf("Size of a char :%d byte(s)\n" , sizeof(c));
 
-printf("Size of an int :%ld byte(s)\n" , sizeof(i));
+printf("Size of an int :%d byte(s)\n" , sizeof(i));
 
-printf("Size of a long int :%ld byte(s)\n" , sizeof(li));
+printf("Size of a long int :%d byte(s)\n" , sizeof(li));
 
-printf("Size of a long long int :%ld byte(s)\n" , sizeof(lli));
+printf("Size of a long long int :%d byte(s)\n" , sizeof(lli));
 
-printf("Size of a float :%ld byte(s)\n" , sizeof(f));
+printf("Size of a float :%d byte(s)\n" , sizeof(f));
 
 return(0);
 
